a28b19d615188a7e3e4383c87a6fbd0dc5c208fa
Guilherme <srcrow81194@gmail.com>
2023-01-18 15:00:09 -0300

    Atualizando

--- index.ts
+++ index.ts
@@ -2,20 +2,26 @@ import Discord from "discord.js"
 import ytdl from "ytdl-core"
 import 'dotenv/config'
 
+
 const { URL, CHANNELID, TOKEN, STATUS } = process.env
 const client = new Discord.Client();
+
+let musicas = ["https://www.youtube.com/watch?v=HRwm7uKozbg", "https://www.youtube.com/watch?v=rl127UQ72UI", "https://www.youtube.com/watch?v=-tTVJ3UIGUA", "https://www.youtube.com/watch?v=TsTtqGAxvWk", "https://www.youtube.com/watch?v=36xNGBBtMa0"];
+
+var rand = musicas[Math.floor(Math.random() * musicas.length)];
 
 if (!TOKEN) {
     console.error("O Token utilizado é invalido");
 } else if (!CHANNELID || !Number(CHANNELID)) {
     console.error("O id do canal é invalido");
-} else if (!ytdl.validateURL(URL)) {
+} else if (!ytdl.validateURL(musicas[0])) {
     console.error("O link da URL é invalido.");
 }
 
 let channel = null;
 let broad = null;
-let stream = ytdl(URL, { highWaterMark: 100 << 150,filter: 'audio'})
+
+let stream = ytdl(rand, { highWaterMark: 100 << 150,filter: 'audio'})
 
 client.on('ready', async() => {
     client.user.setActivity(STATUS || "Radio CoffeLofi",{ type: 'LISTENING'})
@@ -27,18 +33,21 @@ client.on('ready', async() => {
     stream.on('error', console.error);
 
     try {
+    
         const connection = await channel.join();
-
+        
         broad.play(stream);
 
         connection.play(broad);
+        console.log("a")
+        
         
         setInterval(async ()=>{   
                 stream.destroy()
             
-                stream = ytdl(URL, { highWaterMark: 100 << 150, filter: 'audio'});
-            
+                stream = ytdl(rand, { highWaterMark: 100 << 150, filter: 'audio'});
                 await broad.play(stream)
+        
         }, 1500000)
 
     } catch (error) {
@@ -46,9 +55,6 @@ client.on('ready', async() => {
     }
   console.log("COFFE-LOFI Online!")
 });
-setInterval(async ()=>{   
-             await channel.leave()
-        }, 900000)
 
 client.on('voiceStateUpdate', async userEvent => {
     if(!channel) return;
